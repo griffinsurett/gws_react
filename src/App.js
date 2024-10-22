34579f9004317c9d0227f0c812acc9610cb4bb20
griffinjoshs <leveloutusa@gmail.com>
2024-10-22 16:52:50 -0400

    fix clisent

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SiteView from './themes/HighTechNeon/index'
+
+function App() {
+    return (
+        <div className="App">
+        <SiteView />
+        </div>
+    );
+}
+
+export default App;
